70aba3897ef7da52a59d3fdc2333b0c4b79bf2f8
poptar-sommelier <rich.d.nospam@gmail.com>
2020-02-01 00:04:04 -0500

    added flask-bootstrap, changed base.html

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 # import MySQLdb
 import pymysql
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models
 
--- fetchlinks_webapp.py
+++ fetchlinks_webapp.py
@@ -1,5 +1,4 @@
 # TODO: CONFIGURE LOGGING
-# TODO: ON FRONT PAGE: LAST UPDATED TIME
 # TODO: IMPLEMENT FLASK-BOOTSTRAP
 # systemd config
 # https://blog.miguelgrinberg.com/post/running-a-flask-application-as-a-service-with-systemd
